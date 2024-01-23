37ca753ec8dcac118b565ec463c1c3cce90010c8
Antoine07 <antoine.lucsko@gmail.com>
2024-01-23 09:13:02 +0100

    survived

--- Part3_Generator/Exercices/02_titanic_survived.py
+++ Part3_Generator/Exercices/02_titanic_survived.py
@@ -1,4 +1,9 @@
 import csv
+import typing
+import csv
+
+# récupération du Modèle pour hydrater un futur fichier de données
+from Model import Passenger 
 
 class Counter:
     def __init__(self, count = 0):
@@ -6,26 +11,31 @@ class Counter:
         
 c = Counter()
 
-class Passenger:
-    def __init__(self, id, survived,pclass,name,sex,age ):
-        pass
-
 # lecture d'un fichier 
-def gen_titanic(file, age = 41):
+def gen_titanic(file, age = 41, count = 0):
     with open(file, 'r') as csv_file:
+        # le quotechar='|' pour qu'il ne retire pas les doubles guillemets (sinon il ajour des éléments dans la liste, voir les données)
         spamreader = csv.reader(csv_file, delimiter=',', quotechar='|')
         for row in spamreader:
-            if ( row[1] in ('0', '1') and row[1]  == '1' ) and ( "female" in row ) and ( row[6] != '' and float( row[6] ) < 41 ) :
+            if ( row[1] in ('0', '1') and row[1]  == '1' ) \
+            and ( "female" in row ) \
+            and ( row[6] != '' and float( row[6] ) < age ) :
+                count += 1
                 yield {"id" : row[0], "name" : row[3], "age" : float( row[6] ) }
-    
+    c.count = count
             
 g = gen_titanic("./Data/titanic.csv")
 
+def save(P : Passenger) -> None :
+    pass
 
 try:
     while True:
         res = next(g)
+        res = { 'survived' : 1, 'sex' : 'F', **res }
         print(res)
 except StopIteration as e:
-    print("stop", c.count )
+    # Objet Count pour compter lors des traitements sur les données avec le générateur
+    print( f"Nombre de personne répondant aux critères : {c.count}") 
     
+
